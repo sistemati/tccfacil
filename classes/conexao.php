7e527d0e2b03aa0c51bf21009e279a4adb5c462e
Anderson Siqueira <contato@andersonls.com.br>
2020-11-25 20:54:03 -0300

    Alteração no nome do banco na conexão

--- classes/conexao.php
+++ classes/conexao.php
@@ -10,7 +10,7 @@ function conectar()
 	$hostname = 'localhost';
 	$username = 'root';
 	$password = '';
-	$database = 'tccfacil2';
+	$database = 'tccfacil';
 
 	try {
 		$opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');
